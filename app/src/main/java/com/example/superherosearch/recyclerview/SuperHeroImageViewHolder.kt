07815759673209377588c3236f476f2a860dcbd6
kennyli <kennyli138@gmail.com>
2020-12-02 20:12:23 -0800

    Added logic to populate super hero recycler view with name and image

--- app/src/main/java/com/example/superherosearch/recyclerview/SuperHeroImageViewHolder.kt
+++ app/src/main/java/com/example/superherosearch/recyclerview/SuperHeroImageViewHolder.kt
@@ -1,12 +1,25 @@
 package com.example.superherosearch.recyclerview
 
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.superherosearch.R
 import com.example.superherosearch.databinding.ViewSuperHeroBinding
 
 class SuperHeroImageViewHolder(
     private val viewBinding: ViewSuperHeroBinding
 ) : RecyclerView.ViewHolder(viewBinding.root) {
+  companion object {
+    private const val TIMEOUT_IN_SECONDS = 30000
+  }
+
   fun bind(item: SuperHeroItem.Image) {
-    // TODO: populate with data
+    viewBinding.superHeroName.text = item.name
+
+    Glide.with(viewBinding.root)
+      .load(item.imageUrl)
+      .timeout(TIMEOUT_IN_SECONDS)
+      .placeholder(R.drawable.ic_wallpaper_24dp)
+      .error(R.drawable.ic_error_outline_24dp)
+      .into(viewBinding.superHeroImage)
   }
 }
