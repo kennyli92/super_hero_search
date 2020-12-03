2919b81285d897a9efe60e0364e8de1f8b86be61
kennyli <kennyli138@gmail.com>
2020-12-02 18:06:58 -0800

    Added hilt dependency. Created initial hilt setup

---
+++ app/src/main/java/com/example/superherosearch/dagger/ActivityModule.kt
@@ -0,0 +1,9 @@
+package com.example.superherosearch.dagger
+
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+
+@InstallIn(ActivityComponent::class)
+class ActivityModule {
+
+}
